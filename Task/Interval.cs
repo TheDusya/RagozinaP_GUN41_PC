f7019b3e839a6563e0b6485b60113359749761b3
Polina R <polly.poem@gmail.com>
2026-02-04 22:01:23 +0300

    Interval and Room added

---
+++ Task/Interval.cs
@@ -0,0 +1,35 @@
+﻿namespace Task
+{
+    internal struct Interval
+    {
+        private int _min;
+        public int Min { get => _min; }
+        private int _max;
+        public int Max { get => _max; }
+        private Random randomizer = new Random();
+        public int Get => randomizer.Next(Min, Max);
+        public Interval(int minValue, int maxValue)
+        {
+            if (minValue > maxValue)
+            {
+                Console.WriteLine($"WARNING! The min damage value for interval ({minValue}) was greater than max value ({maxValue}), so we switched them. Be more attentive!");
+                (minValue, maxValue) = (maxValue, minValue);
+            }
+            if (minValue < 0) 
+            {
+                minValue = 0;
+                Console.WriteLine($"WARNING! minValue was negative. It was changed to zero.");
+            }
+            if (maxValue < 0) 
+            {
+                maxValue = 0;
+                Console.WriteLine($"WARNING! maxValue was negative. It was changed to zero.");
+            }
+            if (maxValue == minValue) 
+            {
+                maxValue += 10;
+                Console.WriteLine($"WARNING! maxValue was equal to minValue.");
+            }
+        }
+    }
+}
---
+++ Task/Room.cs
@@ -0,0 +1,13 @@
+﻿namespace Task
+{
+    internal struct Room
+    {
+        public Unit Unit;
+        public Weapon Weapon;
+        public Room(Unit unit, Weapon weapon)
+        {
+            this.Unit = unit;
+            this.Weapon = weapon;
+        }
+    }
+}
