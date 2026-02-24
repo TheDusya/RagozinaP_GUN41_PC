fb603c665da613cfaff89be80ce5a155b06e19c3
Polina R <polly.poem@gmail.com>
2026-02-24 05:55:18 +0300

    base for service created

---
+++ Task/SaveLoadService/FileSystemSaveLoadService.cs
@@ -0,0 +1,19 @@
+﻿namespace Task.SaveLoadService
+{
+    internal class FileSystemSaveLoadService : ISaveLoadService<string>
+    {
+        private readonly string _path;
+        public FileSystemSaveLoadService(string path)
+        { 
+            _path = path;
+        }
+        public void Save(string data, string filename)
+        {
+            throw new NotImplementedException();
+        }
+        public string Load(string filename)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Task/SaveLoadService/ISaveLoadService.cs
@@ -0,0 +1,8 @@
+﻿namespace Task.SaveLoadService
+{
+    public interface ISaveLoadService<T>
+    {
+        void Save(T data, string id);
+        T Load(string id);
+    }
+}
